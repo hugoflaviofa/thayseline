f5ea2f99de6db9df7405ea0af255a067a76bfd63
Hugo Flávio <hugoflavio@live.com>
2022-12-13 20:04:51 -0300

    Implementado responsividade do forms

--- src/components/SendEmail.jsx
+++ src/components/SendEmail.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 
-const div = 'mt-8 w-full max-w-xs text-left';
-const p = 'block text-gray-50 text-center text-xl font-bold mb-2';
+const div = 'mt-8 w-10/12 md:w-6/12 text-left mb-5';
+const p = 'block text-gray-50 text-center text-xl md:text-4xl font-bold mb-2';
 const forms = 'bg-slate-700 shadow-md rounded px-8 pt-6 pb-8 mb-4';
-const label = 'block text-gray-50 text-sm font-bold mb-2';
+const label = 'block text-gray-50 text-sm md:text-3xl font-bold mb-2';
 const input = 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline';
 const textarea = 'h-48 text-start shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline';
 const submit = 'bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline';
