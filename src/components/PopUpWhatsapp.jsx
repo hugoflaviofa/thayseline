d28ef7c5697d9cb867c2fe4c614147b6427f7939
Hugo Flávio <hugoflavio@live.com>
2022-12-23 19:06:32 -0300

    Adicionado hover ao css do button da pop-up e implementado teste com css puro afim de verificar compatibilidade com deploy no Heroku

--- src/components/PopUpWhatsapp.jsx
+++ src/components/PopUpWhatsapp.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import whatsappButton from '../images/icons/whatsapp_icon.svg'
+import React, { useState } from 'react';
+import whatsappButton from '../images/icons/whatsapp_icon.svg';
+import './PopUpWhatsapp.css';
 
 // const popUp = 'absolute bg-rose-600 p-16 flex';
 
@@ -9,12 +10,13 @@ export default function PopUpWhatsapp(props) {
 
   return (
     <div
-      className={`${props.hidden} bottom-1/3 bg-gray-600 p-8 rounded-lg shadow-2xl duration-500`}
+      // className={`${props.hidden} bottom-1/3 bg-gray-600 p-8 rounded-lg shadow-2xl duration-500`}
+      className={`teste${props.hidden}`}
     >
       <button
         type="button"
-        className="absolute -top-5 right-0 w-10 self-end bg-white rounded-full p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-        onClick={() => props.setOpacity('hidden')}
+        className="absolute -top-5 right-0 w-10 self-end bg-white rounded-full p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+        onClick={() => props.setOpacity('')}
       >
         <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
@@ -36,7 +38,7 @@ export default function PopUpWhatsapp(props) {
         ? `https://wa.me/5511996887972?text=Olá,%20me%20chamo%20${name}.%20Gostaria%20de%20saber%20mais%20sobre%20treino%20personalizado.`
         : 'https://wa.me/5511996887972?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20treino%20personalizado.'
       } target='blank'>
-        <div className='flex w-44 md:w-64 bg-green-500 m-auto rounded-lg mt-6 shadow-2xl'>
+        <div className='flex w-44 md:w-64 bg-green-500 hover:bg-green-700 duration-300 m-auto rounded-lg mt-6 shadow-2xl'>
           <img className='w-14' alt="Chat para o WhatsApp!" src={whatsappButton} />
           <p className='w-60 pr-4 m-auto'>Inicie o Chat</p>
         </div>
--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -6,7 +6,7 @@ const button = 'w-12 md:w-32 bottom-4 md:bottom-8 right-2.5 md:right-10 fixed md
 
 export default function WhatsappButton() {
 
-  const [opacity, setOpacity] = useState('hidden');
+  const [opacity, setOpacity] = useState('');
 
   // const changeHidden = () => {
   //   const value = opacity ? 0 : 100;
@@ -19,7 +19,7 @@ export default function WhatsappButton() {
       {/* <a href='https://wa.me/5511996887972?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20treino%20personalizado.' target='blank'>
         <img src={whatsappButton} alt='Botão de contato para WhatsApp' className={button} />
       </a> */}
-      <button onClick={() => setOpacity('fixed flex flex-col gap-y-2')}>
+      <button onClick={() => setOpacity(' teste2')}>
         <img src={whatsappButton} alt='Botão de contato para WhatsApp' className={button} />
       </button>
     </>
